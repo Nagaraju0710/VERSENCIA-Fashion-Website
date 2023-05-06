d18a2d4fef6a02cf0236fe6d5865250a6be7d1fa
Nagaraju0710 <nagarajulonka1999@gmail.com>
2023-05-06 07:47:51 +0530

    Home page done

--- fashionstar/src/Components/Navbar.jsx
+++ fashionstar/src/Components/Navbar.jsx
@@ -4,7 +4,7 @@ import {Link } from "react-router-dom";
 function Navbar(){
 return (
 <div className="Navbar" style={{margin:"10px", padding:'30px',boxShadow: '1px 2px 9px #F4AAB9',width:"95%"}}>
-<Link style={{textDecoration: 'none'}} to="/">Dribbble</Link>
+<Link style={{textDecoration: 'none',fontSize:"large",fontFamily:"serif",color:"deeppink"}} to="/">Dribbble</Link>
 <Link style={{textDecoration: 'none',paddingLeft:"50px"}} to="/inspiration">Inspiration</Link>
 <Link style={{textDecoration: 'none',paddingLeft:"50px"}} to="/findwork">Find Work</Link>
 <Link style={{textDecoration: 'none',paddingLeft:"50px"}} to="/learndesign">Learn Design</Link>
--- fashionstar/src/Pages/Dribbble.jsx
+++ fashionstar/src/Pages/Dribbble.jsx
@@ -3,58 +3,192 @@ import React from 'react';
 // import logo from "./logo.png";
 const Dribbble = () => {
     return (
-        <div>
-            <div style={{ display: 'flex', justifyContent: "center", padding: "50px", fontFamily: "sans-serif", height: "30px" }}>
 
-                <img style={{ width: "50px", height: "50px", paddingRight: "20px", paddingTop: "20px" }} src="https://cdn.dribbble.com/users/4214551/avatars/normal/c210b223d794fadb3480fc846cef0c86.jpg?1613608362" alt="" />
+        <div style={{display:"flex"}}>
+            
+            <div >
+                <div style={{ display: 'flex', justifyContent: "center", padding: "50px", fontFamily: "sans-serif", height: "30px" }}>
+
+                    <a href=""><img style={{ width: "50px", height: "50px", paddingRight: "20px", paddingTop: "20px" }} src="https://cdn.dribbble.com/users/4214551/avatars/normal/c210b223d794fadb3480fc846cef0c86.jpg?1613608362" alt="" /></a>
+                    <div>
+                        <h3>VERSENCIA Fashion Website</h3>
+                        <h5 style={{ paddingBottom: "30px" }}>Tosan Garditama ✦ for One Week Wonders
+                            •
+                            Follow
+                            •
+                            Hire Us</h5>
+                    </div>
+                    <div style={{ padding: "20px", margin: "20px" }}>
+                        <button style={{ fontSize: "15px" }}>Save</button>
+                        <button style={{ fontSize: "15px", background: "deeppink", color: "white" }}>Like</button>
+                    </div>
+                </div>
+                <div style={{ marginLeft: "200px" }}>
+                    <video width="1000" height="800" controls >
+                        <source src="https://cdn.dribbble.com/userupload/4486273/file/original-110fd8bd5101c3ea4eae070e1ba34db6.mp4" type="video/mp4" /> </video>
+                </div>
+                <div style={{ textAlign: 'center' }}>
+                    <h1>Hi there everyone!</h1>
+                    <h3>Here is my exploration about the Fashion website.</h3>
+                    <h3>I hope you guys enjoy it!</h3>
+                    <h3 style={{ paddingTop: "20px" }}>The photos in Summer Palette I got from hardclo.com, great products!</h3>
+                </div>
+                <img style={{ marginLeft: "200px", width: "1000px", borderRadius: "20px", paddingTop: "40px" }} src="https://cdn.dribbble.com/userupload/4486274/file/original-4e0aa6b1fa64725d475ae2877735eb50.png?compress=1&resize=1200x900&vertical=center" alt="error" />
+                <h1 style={{ textAlign: "center", paddingTop: "50px", fontFamily: "fantasy" }}>Full Version ✦</h1>
+                <img style={{ marginLeft: "200px", width: "1000px", paddingTop: "50px" }} src="https://cdn.dribbble.com/userupload/4486275/file/original-e67cb658a417a154120a2ee256e172cb.png?compress=1&resize=1200x3400&vertical=center" alt="error" />
+                <div style={{ textAlign: "center", paddingTop: "50px" }}>
+                    <h1>So, do you think this is cool?</h1>
+                    <h3 style={{ fontFamily: "sans-serif", fontSize: "light" }}>Feel free to feedback and comment.</h3>
+                    <h3 style={{ fontFamily: "sans-serif", fontSize: "light" }}>Don't forget to press "L" if love it.</h3>
+                    <h3 style={{ fontFamily: "sans-serif", fontSize: "light" }}>Thanks!</h3>
+                </div>
+                <div style={{ textAlign: "center", paddingTop: "50px" }}>
+                    <h2>------------------------------------------------------</h2>
+                    <h3>Connect with us : owwstudio@gmail.com</h3>
+                    <h2>------------------------------------------------------</h2>
+                    <h3> Learn more about us : <a href="" style={{ textDecoration: "none", color: "deeppink" }}>www.oneweekwonders.com</a></h3>
+                    <h2>------------------------------------------------------</h2>
+                    <h3><a href="" style={{ textDecoration: "none", color: "deeppink" }}>Instagram | Behance | Shop at UI8</a></h3>
+
+                </div>
+                <div style={{ textAlign: "center", paddingTop: "50px" }}>
+                    <hr />
+                    <img style={{ borderRadius: "50px" }} src="https://cdn.dribbble.com/users/1667332/avatars/small/edd45777913e5de4434a1a1b812c76cf.png?1566878129" alt="error" />
+                    <h4>One Week Wonders</h4>
+                    <h6>Agency with incredible design and development expertise</h6>
+                    <button style={{ background: "deeppink", color: "white", height: '30px', width: "80px", borderRadius: "5px" }}>Hire Us</button>
+                </div>
+                <div style={{ display: "flex", justifyContent: "space-between", marginTop: "50px" }}>
+                    <h3 style={{ marginLeft: "30px" }}>More by One Week Wonders</h3>
+                    <a style={{ textDecoration: "none", color: "deeppink" }} href="">View Products</a>
+                </div>
+
+                <div style={{ display: "flex", justifyContent: "space-between", margin: "20px" }}>
+
+                    <div >
+                        <img style={{ borderRadius: "30px" }} src="https://cdn.dribbble.com/users/2947819/screenshots/18101199/media/29d0938c6fd72906862d4174baf11605.png?compress=1&resize=320x240&vertical=top" alt="error" />
+                    </div>
+                    <div>
+                        <img style={{ borderRadius: "30px" }} src="https://cdn.dribbble.com/userupload/4879595/file/original-8ca338e645b0bc0830a73a3adc5dc2e7.png?compress=1&resize=320x240&vertical=top" alt="error" />
+                    </div>
+                    <div>
+                        <img style={{ borderRadius: "30px" }} src="https://cdn.dribbble.com/userupload/4166487/file/original-51ac09180f60dae461e9742a000f6a48.png?compress=1&crop=0x0-1600x1200&resize=320x240&vertical=top" alt="error" />
+                    </div>
+                    <div>
+                        <img style={{ borderRadius: "30px" }} src="https://cdn.dribbble.com/userupload/4646835/file/original-1b3cd350e25e1d6deb35721c406ef383.png?compress=1&resize=320x240&vertical=top" alt="error" />
+                    </div>
+                </div>
+                <hr />
+                <div style={{ display: 'flex', justifyContent: "space-between", margin: "15px", gap:"20px"}}>
+                    <div>
+                        <a href=""><img style={{ borderRadius: "20px" }} src="https://cdn.dribbble.com/userupload/4254605/file/original-51e9cb0f4cd7d3d253516595cd0737a6.png?compress=1&resize=450x338&vertical=top" alt="" /></a>
+                    </div>
+                    <div>
+                        <a href=""><img style={{ borderRadius: "20px" }} src="https://cdn.dribbble.com/users/2215956/screenshots/17732397/kove_interactions_dribbble.png?compress=1&resize=450x338&vertical=top" alt="" /></a>
+                    </div>
+                    <div>
+                        <a href=""><img style={{ borderRadius: "20px" }} src="https://cdn.dribbble.com/userupload/4869554/file/original-35e67ab84073368636947cae6e5908a1.png?compress=1&resize=450x338&vertical=top" alt="" /></a>
+                    </div>
+                </div>
+                <div style={{ display: "flex", justifyContent: "space-between", margin: "15px", gap:"20px"}}>
+                    <div>
+                        <a href=""><img style={{ width: "450px", borderRadius: "20px" }} src="https://cdn.dribbble.com/userupload/2904322/file/original-5184e92e56d1a19ba2568a9fc8de3656.png?compress=1&resize=640x480&vertical=top" alt="" /></a>
+                    </div>
+                    <div>
+                        <a href=""><img style={{ width: "450px", borderRadius: "20px" }} src="https://cdn.dribbble.com/userupload/3060512/file/still-0e11b7a5a7c2dab58956fa2f917c7bac.png?compress=1&resize=640x480&vertical=top" alt="" /></a>
+                    </div>
+                    <div>
+                        <a href=""><img style={{ borderRadius: "20px" }} src="https://cdn.dribbble.com/userupload/4254605/file/original-51e9cb0f4cd7d3d253516595cd0737a6.png?compress=1&resize=450x338&vertical=top" alt="" /></a>
+                    </div>
+                </div>
+                <hr />
+                <div style={{ display: "flex", justifyContent: "space-between", width: "100%", backgroundColor: "azure", marginTop: "150px" }}>
+                    <div>
+                        <h1 style={{ fontFamily: "serif", color: "deeppink" }}>dribbble</h1>
+                        <p>Dribbble is the world’s leading</p>
+                        <p>community for creatives to share, grow,</p>
+                        <p> and get hired.</p>
+                        <a href="https://dribbble.com/dribbble"><img style={{ width: "25px" }} src="https://cdn.dribbble.com/users/39/avatars/normal/f342d2c8d6677c536875e7de73e481ee.png?1582211833" alt="error" /></a>
+                        <a href="https://twitter.com/dribbble"><img style={{ width: "30px" }} src="https://about.twitter.com/content/dam/about-twitter/en/brand-toolkit/brand-download-img-1.jpg.twimg.1920.jpg" alt="error" /></a>
+                        <a href="https://www.facebook.com/dribbble"><img style={{ width: "50px" }} src="https://1000logos.net/wp-content/uploads/2016/11/fb-logo.jpg" alt="error" /></a>
+                        <a href="https://www.instagram.com/dribbble/"><img style={{ width: "30px" }} src="https://img.freepik.com/premium-vector/purple-gradiend-social-media-logo_197792-1883.jpg?w=2000" alt="error" /></a>
+                        <a href="https://www.pinterest.com/dribbble/"><img style={{ width: "30px" }} src="https://png.pngtree.com/png-clipart/20190516/original/pngtree-pinterest-social-media-icon-design-template-vector-png-image_3654773.jpg" alt="error" /></a>
+                    </div>
+                    <div>
+                        <h4>For designers</h4>
+                        <a style={{ textDecoration: "none", color: "black" }} href=""><p>Go Pro!</p></a>
+                        <a style={{ textDecoration: "none", color: "black" }} href=""><p>Explore design work</p></a>
+                        <a style={{ textDecoration: "none", color: "black" }} href=""><p>Design blog</p></a>
+                        <a style={{ textDecoration: "none", color: "black" }} href=""><p>Overtime podcast</p></a>
+                        <a style={{ textDecoration: "none", color: "black" }} href=""><p>Playoffs</p></a>
+                        <a style={{ textDecoration: "none", color: "black" }} href=""><p>Code of conduct</p></a>
+                    </div>
+                    <div>
+                        <h4>Hire designers</h4>
+                        <a style={{ textDecoration: "none", color: "black" }} href=""><p>Post a job opening</p></a>
+                        <a style={{ textDecoration: "none", color: "black" }} href=""><p>Post a freelance projectk</p></a>
+                        <a style={{ textDecoration: "none", color: "black" }} href=""><p>Design blog</p></a>
+                        <a style={{ textDecoration: "none", color: "black" }} href=""><p>Search for designers</p></a>
+                        <a style={{ textDecoration: "none", color: "black" }} href=""><p>Brands</p></a>
+                        <a style={{ textDecoration: "none", color: "black" }} href=""><p>Advertise with us</p></a>
+                    </div>
+                    <div>
+                        <h4>Company</h4>
+                        <a style={{ textDecoration: "none", color: "black" }} href=""><p>About</p></a>
+                        <a style={{ textDecoration: "none", color: "black" }} href=""><p>Careers</p></a>
+                        <a style={{ textDecoration: "none", color: "black" }} href=""><p>Support</p></a>
+                        <a style={{ textDecoration: "none", color: "black" }} href=""><p>Media kit</p></a>
+                        <a style={{ textDecoration: "none", color: "black" }} href=""><p>Testimonials</p></a>
+                        <a style={{ textDecoration: "none", color: "black" }} href=""><p>API</p></a>
+                        <a style={{ textDecoration: "none", color: "black" }} href=""><p>Terms of service</p></a>
+                        <a style={{ textDecoration: "none", color: "black" }} href=""><p>Privacy policy</p></a>
+                        <a style={{ textDecoration: "none", color: "black" }} href=""><p>Cookie policy</p></a>
+                    </div>
+                    <div>
+                        <h4>Directories</h4>
+                        <a style={{ textDecoration: "none", color: "black" }} href=""><p>Design jobs</p></a>
+                        <a style={{ textDecoration: "none", color: "black" }} href=""><p>Designers for hire</p></a>
+                        <a style={{ textDecoration: "none", color: "black" }} href=""><p>Freelance designers for hire</p></a>
+                        <a style={{ textDecoration: "none", color: "black" }} href=""><p>Tags</p></a>
+                        <a style={{ textDecoration: "none", color: "black" }} href=""><p>Places</p></a>
+                        <a style={{ textDecoration: "none", color: "black" }} href=""><p>Design assets</p></a>
+                        <a style={{ textDecoration: "none", color: "black" }} href=""><p>Dribbble Marketplace</p></a>
+                        <a style={{ textDecoration: "none", color: "black" }} href=""><p>Creative Market</p></a>
+                        <a style={{ textDecoration: "none", color: "black" }} href=""><p>Fontspring</p></a>
+                        <a style={{ textDecoration: "none", color: "black" }} href=""><p>Font Squirrel</p></a>
+                    </div>
+                    <div>
+                        <h4>Design Resources</h4>
+                        <a style={{ textDecoration: "none", color: "black" }} href=""><p>Freelancing</p></a>
+                        <a style={{ textDecoration: "none", color: "black" }} href=""><p>Design Hiring</p></a>
+                        <a style={{ textDecoration: "none", color: "black" }} href=""><p>Design Portfolio</p></a>
+                        <a style={{ textDecoration: "none", color: "black" }} href=""><p>Design Education</p></a>
+                        <a style={{ textDecoration: "none", color: "black" }} href=""><p>Creative Process</p></a>
+                        <a style={{ textDecoration: "none", color: "black" }} href=""><p>Design Industry Trends</p></a>
+                    </div>
+                </div>
+                <hr />
+            </div>
+            <div style={{paddingTop:"70px" }}>
                 <div>
-                    <h3>VERSENCIA Fashion Website</h3>
-                    <h5 style={{ paddingBottom: "30px" }}>Tosan Garditama ✦ for One Week Wonders
-                        •
-                        Follow
-                        •
-                        Hire Us</h5>
+                    <a href=""><img style={{ width: "50px", height: "50px", paddingRight: "20px" }} src="https://cdn.dribbble.com/users/4214551/avatars/normal/c210b223d794fadb3480fc846cef0c86.jpg?1613608362" alt="" /></a>
                 </div>
-                <div style={{ padding: "20px", margin: "20px" }}>
-                    <button style={{ fontSize: "15px" }}>Save</button>
-                    <button style={{ fontSize: "15px", background: "deeppink", color: "white" }}>Like</button>
+                <div>
+                    <a href=""><img style={{ width: "50px", height: "50px", paddingRight: "20px" }} src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcStd0cSzItBn3mUwXeOnjYZWg25hj28SquCh6BJIquPqbz3LI3x" alt="error" /></a>
+                </div>
+                <div>
+                    <a href=""><img style={{ width: "50px", height: "50px", paddingRight: "20px" }} src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSfUFezihrxXpR25nXJ9o2lrIjAE75cFGbV7fUV7sRu0VXSgid9" alt="error" /></a>
+                </div>
+                <div>
+                    <a href=""><img style={{ width: "50px", height: "50px", paddingRight: "20px" }} src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQMz9RWoTfXWJLOQeKOIsxZtKr_8ohz3sIY0oQGLjAg2lj6ol7E" alt="error" /></a>
+                </div>
+                <div>
+                    <a href=""><img style={{ width: "50px", height: "50px", paddingRight: "20px" }} src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQ1oWpaAHO3SVUrWdJpbz8hlgGObNnbZCPfQt1g7MoxrzDnLxmA" alt="error" /></a>
+                </div>
+                <div>
+                    <a href=""><img style={{ width: "50px", height: "50px", paddingRight: "20px" }} src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTRUKEXnhi5HdlJqHUfBlN8hMois9mcQSJPQfd2z-IWFAjZmLrC" alt="error" /></a>
                 </div>
             </div>
-            <div style={{ marginLeft: "200px" }}>
-                <video width="1000" height="800" controls >
-                    <source src="https://cdn.dribbble.com/userupload/4486273/file/original-110fd8bd5101c3ea4eae070e1ba34db6.mp4" type="video/mp4" /> </video>
-            </div>
-            <div style={{ textAlign: 'center' }}>
-                <h1>Hi there everyone!</h1>
-                <h3>Here is my exploration about the Fashion website.</h3>
-                <h3>I hope you guys enjoy it!</h3>
-                <h3 style={{ paddingTop: "20px" }}>The photos in Summer Palette I got from hardclo.com, great products!</h3>
-            </div>
-            <img style={{ marginLeft: "200px", width: "1000px", borderRadius: "20px", paddingTop: "40px" }} src="https://cdn.dribbble.com/userupload/4486274/file/original-4e0aa6b1fa64725d475ae2877735eb50.png?compress=1&resize=1200x900&vertical=center" alt="error" />
-            <h1 style={{ textAlign: "center", paddingTop: "50px", fontFamily: "fantasy" }}>Full Version ✦</h1>
-            <img style={{ marginLeft: "200px", width: "1000px", paddingTop: "50px" }} src="https://cdn.dribbble.com/userupload/4486275/file/original-e67cb658a417a154120a2ee256e172cb.png?compress=1&resize=1200x3400&vertical=center" alt="error" />
-            <div style={{ textAlign: "center", paddingTop: "50px" }}>
-                <h1>So, do you think this is cool?</h1>
-                <h3 style={{ fontFamily: "sans-serif", fontSize: "light" }}>Feel free to feedback and comment.</h3>
-                <h3 style={{ fontFamily: "sans-serif", fontSize: "light" }}>Don't forget to press "L" if love it.</h3>
-                <h3 style={{ fontFamily: "sans-serif", fontSize: "light" }}>Thanks!</h3>
-            </div>
-            <div style={{ textAlign: "center", paddingTop: "50px" }}>
-                <h2>------------------------------------------------------</h2>
-                <h3>Connect with us : owwstudio@gmail.com</h3>
-                <h2>------------------------------------------------------</h2>
-                <h3> Learn more about us : <a href="" style={{ textDecoration: "none", color: "deeppink" }}>www.oneweekwonders.com</a></h3>
-                <h2>------------------------------------------------------</h2>
-                <h3><a href="" style={{ textDecoration: "none", color: "deeppink" }}>Instagram | Behance | Shop at UI8</a></h3>
-
-            </div>
-        <div style={{textAlign:"center", paddingTop: "50px" }}>
-            <hr />
-            <img style={{borderRadius:"50px"}} src="https://cdn.dribbble.com/users/1667332/avatars/small/edd45777913e5de4434a1a1b812c76cf.png?1566878129" alt="error" />
-            <h4>One Week Wonders</h4>
-            <h6>Agency with incredible design and development expertise</h6>
-            <button style={{background:"deeppink",color:"white",height:'30px',width:"80px",borderRadius:"5px"}}>Hire Us</button>
-        </div>
         </div>
     );
 };
